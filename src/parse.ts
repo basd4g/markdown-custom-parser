87879d4c66adf73294ff8a23e9c99847a4c3989f
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-05-02 11:12:53 +0900

    Support footnotes

--- src/parse.ts
+++ src/parse.ts
@@ -1,7 +1,8 @@
 import remark from 'remark';
 import html from 'remark-html';
+import footnotes from 'remark-footnotes';
 
 export default async function markdownToHtml(markdown: any) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(footnotes).use(html).process(markdown);
   return result.toString();
 }
